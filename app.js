78293b59c2c7b16c6aa98d8f6febc3ec77365db5
RaquelWriter <raquel@mundofeliz.com>
2023-03-13 12:16:07 +0100

    feat: add right uri

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const { MongoClient } = require('mongodb'); // import the MongoClient
 const uri = require('./atlas_uri'); // import the url string for the connection
-
+/*
+ atlas_uri will contain:
+ change <myDBUser> and <password> for your dbUser and DB password
+module.exports = uri =
+  'mongodb+srv://<myDBUser>:<password>@myatlasclusteredu.waapigk.mongodb.net/?retryWrites=true&w=majority';
+*/
 console.log(uri);
 
 // Using the MongoClient to initiate the mongo connection to the DB
@@ -18,3 +23,33 @@ const connectToDatabase = async () => {
   }
 };
 connectToDatabase();
+
+/* 
+SAMPLE FOR insertOne()
+
+const dbname = 'bank';
+const collection_name = 'accounts';
+
+const accountsCollection = client.db(dbname).collection(collection_name);
+
+const sampleAccount = {
+  account_holder: 'Linus Torvalds',
+  account_id: 'MDB829001337',
+  account_type: 'checking',
+  balance: 50352434,
+};
+
+const main = async () => {
+  try {
+    await connectToDatabase();
+    // insertOne method is used here to insert the sampleAccount document
+    let result = await accountsCollection.insertOne(sampleAccount);
+    console.log(`Inserted document: ${result.insertedId}`);
+  } catch (err) {
+    console.error(`Error inserting document: ${err}`);
+  } finally {
+    await client.close();
+  }
+};
+
+main(); */
--- atlas_uri.js
+++ atlas_uri.js
@@ -1,2 +1,2 @@
 module.exports = uri =
-  'mongodb+srv://<username>:<password>@myatlasclusteredu.3a6raid.mongodb.net/?retryWrites=true&w=majority';
+  'mongodb+srv://myAtlasDBUser:mmR3PTRSN2VKjPR9@myatlasclusteredu.waapigk.mongodb.net/?retryWrites=true&w=majority';
